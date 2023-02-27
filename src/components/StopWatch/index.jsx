8ddc5d519a9df0444d308d41be3980a0ee6ec895
Ivan Vlasov <vlasoff.ivan95@gmail.com>
2023-02-28 00:52:55 +0200

    changes

--- src/components/StopWatch/index.jsx
+++ src/components/StopWatch/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import styles from './Timer.module.css'
+import styles from "./Timer.module.css";
 
 const stateInitial = {
   startNumber: 0,
@@ -38,13 +38,13 @@ class StopWatch extends Component {
   };
 
   addLaps = () => {
-    if(this.state.statusWatch) {
-    const { startNumber, laps } = this.state;
-    this.setState({
-      laps: [...laps, startNumber],
-      
-    });
-  }};
+    if (this.state.statusWatch) {
+      const { startNumber, laps } = this.state;
+      this.setState({
+        laps: [...laps, startNumber],
+      });
+    }
+  };
 
   componentDidMount() {
     this.timerID = setInterval(this.startInterval, 1000);
@@ -60,19 +60,36 @@ class StopWatch extends Component {
 
   render() {
     const { startNumber, statusWatch, laps } = this.state;
-    const lapsArray  = laps.map( (number, index=1) => <li key ={index + 1}> Laps#{index + 1} Time:{number}second </li>);
+    const lapsArray = laps.map((number, index = 1) => (
+      <li key={index + 1}>
+        {" "}
+        Laps#{index + 1} Time:{number}second{" "}
+      </li>
+    ));
     return (
-      <div>
-        <h1>Timer</h1>
-        <h2>{startNumber}</h2>
-        <div className= {styles.btnContainer}>
-        <button className={styles.btnClick} onClick={statusWatch ? this.stopWatch : this.startWatch}>
-          {statusWatch ? "Stop" : "Start"}
-        </button>
-        <button className= {styles.btnClick}onClick={this.resetWatch}>Reset</button>
-        <button className = {styles.btnClick} onClick={this.addLaps}>Lap</button>
-        <ul>{lapsArray}</ul>
-        </div>
+      <div className={styles.bigContainer}>
+        <section className={styles.sectionTimer}>
+          <h1 className={styles.h1Timer}>Timer</h1>
+          <h2 className={styles.h2Timer}>{startNumber}</h2>
+          <div className={styles.btnContainer}>
+            <button
+              className={styles.btnClick}
+              onClick={statusWatch ? this.stopWatch : this.startWatch}
+            >
+              {statusWatch ? "Stop" : "Start"}
+            </button>
+            <button className={styles.btnClick} onClick={this.resetWatch}>
+              Reset
+            </button>
+            <button className={styles.btnClick} onClick={this.addLaps}>
+              Lap
+            </button>
+          </div>
+        </section>
+
+        <section className={styles.lapsStyle}>
+          <ul>{lapsArray}</ul>
+        </section>
       </div>
     );
   }
